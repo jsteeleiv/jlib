63163b3a5b06b31237406d7037c03b6677e4a6f3
jsteeleiv <jsteele101010@gmail.com>
2026-03-12 19:57:27 -0400

    updated jlib header file

--- c/include/jlib/jlib.h
+++ c/include/jlib/jlib.h
@@ -2,19 +2,23 @@
 	cerebr4l 2023
 	
 	jlib.h - jay's library;
-	a silly Umbrella header file to handle some(most) of my default redundancy and try clever, helpful stuff.
-	Likely never to see the light of day...	
-    
+	a silly `Umbrella Header` file to handle some(most) of my default redundancy 
+        and try a bunch of clever, buggy, but hopefully helpful stuff.
+
+	> Likely to never see the light of day...	
+
 */
 
 #ifndef JLIB_H
 #define JLIB_H
 
-#include <jlib/jvec.h>
-#include <jlib/jusb.h>
-#include <jlib/jlogs.h>
+#include <jlib/jansi.h>
+#include <jlib/jconv.h>
 #include <jlib/jcore.h>
 #include <jlib/jlist.h>
+#include <jlib/jlogs.h>
 #include <jlib/jtime.h>
+#include <jlib/jusb.h>
+#include <jlib/jvec.h>
 
 #endif /* JLIB_H */
