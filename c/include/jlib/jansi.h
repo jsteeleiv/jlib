cb277c333ca2c0a6abc882b3c2b37a8a1041aa9f
jsteeleiv <jsteele101010@gmail.com>
2026-03-12 19:18:39 -0400

    update the ansi lib

--- c/include/jlib/jansi.h
+++ c/include/jlib/jansi.h
@@ -1,9 +1,19 @@
-#ifndef J_HUE_H
-#define J_HUE_H
+#ifndef JANSI_H
+#define JANSI_H
 
 #include <stdio.h>
+#include <jlib/jlib.h>
 
-typedef enum {
+#define JANSI_NONE 0
+
+typedef enum Mode {
+    M_NULL = 0,
+    M_8BIT,
+    M_256,
+    M_RGB
+} mode;
+
+typedef enum Code {
     BLACK  = 30,
     RED    = 31,
     GREEN  = 32,
@@ -11,8 +21,48 @@ typedef enum {
     BLUE   = 34,
     PURPLE = 35,
     CYAN   = 36,
-    WHITE  = 37
+    WHITE  = 37,
+    /* bright versions */
+    B_BLACK  = 90,
+    B_RED    = 91,
+    B_GREEN  = 92,
+    B_YELLOW = 93,
+    B_BLUE   = 94,
+    B_PURPLE = 95,
+    B_CYAN   = 96,
+    B_WHITE  = 97
+} code;
+
+typedef enum Attr {
+    BOLD    = 1 << 0,
+    DIM     = 1 << 1,
+    ITALIC  = 1 << 2,
+    UNDERLN = 1 << 3,
+    INVERSE = 1 << 4
+} attr;
+
+typedef struct Color {
+    mode mode;
+    code code;
+    // attr attr;
 } color;
+
+typedef struct Style {
+    color fg;
+    color bg;
+    // attr attr;
+    uint32_t fgval;
+    uint32_t bgval;
+    uint8_t fgmod;
+    uint8_t bgmod;
+    unsigned int attrs;
+} style;
+
+typedef struct Object {
+
+} ansi;
+
+Error printj(style fmt, const char *msg, ...);
 
 static inline void jansi_reset(void);
 static inline void jansi_fg(color color, int bold);
