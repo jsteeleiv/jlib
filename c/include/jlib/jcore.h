9411fdfd77ae7c5cc678665a925a3b10f1d011d6
jsteeleiv <jsteele101010@gmail.com>
2026-03-12 02:36:43 -0400

    cleanup jlist lib

--- c/include/jlib/jcore.h
+++ c/include/jlib/jcore.h
@@ -1,8 +1,47 @@
-#ifndef JLIB_CORE_H
-#define JLIB_CORE_H
+#ifndef JCORE_H
+#define JCORE_H
 
-#endif
+#include <stdarg.h>
+#include <jlib/jlib.h>
 
-#ifdef __cplusplus
-//extern "C" {
-#endif
+// xor_swap, echo (or split later)
+void echo(const char* format, ...);
+void _args(const char *init_arg, ...);
+
+
+
+#ifdef JCORE_IMPL
+
+
+// function that takes a string with underscores 
+// and replaces them with variables using printf in the background.
+void echo(const char* format, ...) {
+    va_list args;
+    va_start(args, format);
+
+    while (*format) {
+        if (*format == '_') {
+            int value = va_arg(args, int);
+            printf("%d", value);
+        } else {
+            putchar(*format);
+        }
+        format++;
+    }
+
+    va_end(args);
+}
+
+// handle my function args, by simply passing them through to avoid stdargs conflict???
+/* void _args(const char *init_arg, ...) {
+	va_list args;
+	va_start(args, init_arg);
+	
+	
+} */
+
+
+
+#endif /* JCORE_IMPL */
+
+#endif /* JCORE_H */
