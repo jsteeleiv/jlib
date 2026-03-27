dba6ca45499a47280f20deb00c04093f2204456c
jsteeleiv <jsteele101010@gmail.com>
2026-03-26 20:58:11 -0400

    introduced jmap library

---
+++ c/include/jlib/jmap.h
@@ -0,0 +1,38 @@
+#ifndef JMAP_H
+#define JMAP_H
+
+#include <jlib/jlib.h>
+
+typedef struct Entry {
+    Jtype val;
+    char *key;
+    size_t hash;
+} entry_t;
+
+typedef struct Buckets {
+    Jlist *entries;
+    size_t size;        // amount of buckets in list
+} bucket_t;
+
+typedef struct Map {
+    size_t size;
+    size_t cap;        // max buckets
+    bucket_t *buckets;
+} map_t;
+
+typedef struct {
+    Jerror errs;
+    Jlog logs;
+    map_t *map;
+} Jmap;
+
+
+
+
+#endif /* JMAP_H */
+
+#ifdef JMAP_IMPL
+
+
+
+#endif /* JMAP_IMPL */
