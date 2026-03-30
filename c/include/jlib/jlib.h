4adece343f239b900f46a9d1482a1dcf3fdaf7a1
jsteeleiv <jsteele101010@gmail.com>
2026-03-30 14:43:41 -0400

    updated to reflect new headers

--- c/include/jlib/jlib.h
+++ c/include/jlib/jlib.h
@@ -1,19 +1,22 @@
+#pragma once
+#ifndef JLIB_H
+#define JLIB_H
+
 /*
 	cerebr4l 2023
 	
 	jlib.h - jay's library;
 	a silly `Umbrella Header` file to handle some(most) of my default redundancy 
         and try a bunch of clever, buggy, but hopefully helpful stuff.
-
+		purposely over-engineered
 	> Likely to never see the light of day...	
 
 */
 
-#ifndef JLIB_H
-#define JLIB_H
 
 #include <jcore.h>
 #include <jdata.h>
+#include <jio.h>
 #include <jparse.h>
 #include <jtools.h>
 
