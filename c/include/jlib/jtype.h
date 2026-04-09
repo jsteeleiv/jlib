5da3da720361d2ec3c374fdf44bc80b4c569cf74
jsteeleiv <jsteele101010@gmail.com>
2026-04-08 21:09:28 -0400

    upgraded jtype to master header level

---
+++ c/include/jlib/jtype.h
@@ -0,0 +1,29 @@
+#pragma once
+#ifndef JTYPE_H
+#define JTYPE_H
+
+#include "jtype/jany.h"
+#include "jtype/jdef.h"
+#include "jtype/jkind.h"
+#include "jtype/jmisc.h"
+#include "jtype/jstr.h"
+#include "jtype/jval.h"
+
+
+// typedef struct {
+//     Jerror err;
+//     uint32_t id;    // debug tracing
+//     value_t val;
+//     kind_t cast;
+// } Jtype;
+
+
+
+#endif /* JTYPE_H */
+
+
+#ifdef JTYPE_IMPL
+
+
+
+#endif /* JTYPE_IMPL */
