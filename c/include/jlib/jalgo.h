f9ea00ade4138a0d32ac024e4142f5f7c5d9dfff
jsteeleiv <jsteele101010@gmail.com>
2026-04-08 21:06:22 -0400

    add; jalgo master header file

---
+++ c/include/jlib/jalgo.h
@@ -0,0 +1,24 @@
+#pragma once
+#ifndef JALGO_H
+#define JALGO_H
+
+/* master `algorithm` header file
+ - will house all the includes and some high level helpers
+*/
+
+
+// #include <jsort.h>
+// #include <jsearch.h>
+
+
+
+
+#endif /* JALGO_H */
+
+#ifdef JALGO_IMPL
+
+
+#endif /* JALGO_IMPL */
+
+
+
