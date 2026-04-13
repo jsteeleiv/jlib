3cedeca5623676b948ab3189c7b86e10624675fd
jsteeleiv <jsteele101010@gmail.com>
2026-04-13 01:17:25 -0400

    introduced casting logic

--- c/include/jlib/jtype/jval.h
+++ c/include/jlib/jtype/jval.h
@@ -2,21 +2,19 @@
 #ifndef JVAL_H
 #define JVAL_H
 
-//#include <stdbool.h>
+#include <stdbool.h>
 #include "jdef.h"
 
 typedef struct ValueType {
     typedef_t type;
+    any_v cast;
 } value_t;
 
 bool val_setcast(value_t *val, kind_t k);
 bool val_castas(value_t *val, kind_t k);
 
 #endif /* JVAL_H */
-
-
 #ifdef JVAL_IMPL
-
 
 bool val_setcast(value_t *val, kind_t k){
     if (!val || !kind_valid(k)) return false;
