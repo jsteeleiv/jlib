1fb330c1c109862401013b77daed1608901ac806
jsteeleiv <jsteele101010@gmail.com>
2026-04-18 19:10:35 -0400

    renamed jparse -> jlex

--- c/include/jlib/jlex/jparse.h
+++ c/include/jlib/jlex/jparse.h
@@ -2,9 +2,23 @@
 #ifndef JPARSE_H
 #define JPARSE_H
 
-#include "jparse/jargs.h"
-#include "jparse/jedit.h"
-#include "jparse/jlex.h"
-#include "jparse/jregx.h"
+
+#include <stdio.h>
+#include <stdbool.h>
+
+typedef enum TokenType {
+    TOKEN_UNKNOWN,
+    TOKEN_WORD,
+    TOKEN_SPACE,
+    TOKEN_NEWLINE,
+} toktype_t;
+
+typedef struct Token {
+    TokenType type;
+    char *value;
+} token_t;
+
+Token getNextToken(FILE *file);
+bool match(const char *pattern, Token *tokens);
 
 #endif /* JPARSE_H */
