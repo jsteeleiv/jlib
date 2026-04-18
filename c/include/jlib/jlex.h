be322c5d55708aec4037165133f4a99669a23380
jsteeleiv <jsteele101010@gmail.com>
2026-04-18 02:45:26 -0400

    restructured into jlex sub-library

---
+++ c/include/jlib/jlex.h
@@ -0,0 +1,18 @@
+#pragma once
+#ifndef JLEX_H
+#define JLEX_H
+
+
+#include "jlex/jargs.h"
+#include "jlex/jedit.h"
+#include "jlex/jparse.h"
+#include "jlex/jregx.h"
+
+#ifdef JLEX_IMPL
+
+
+#endif /* JLEX_IMPL */
+
+
+
+#endif /* JLEX_H */
