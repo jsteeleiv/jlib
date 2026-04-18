3d4a5b994eb818ed677bc87c0d399b7a7578b099
jsteeleiv <jsteele101010@gmail.com>
2026-04-18 00:02:53 -0400

    renamed primary structure

--- c/include/jlib/jtype/jval.h
+++ c/include/jlib/jtype/jval.h
@@ -8,21 +8,21 @@
 typedef struct ValueType {
     typedef_t type;
     any_v cast;
-} value_t;
+} val_t;
 
-bool val_setcast(value_t *val, kind_t k);
-bool val_castas(value_t *val, kind_t k);
+bool val_setcast(val_t *val, kind_t k);
+bool val_castas(val_t *val, kind_t k);
 
 #endif /* JVAL_H */
 #ifdef JVAL_IMPL
 
-bool val_setcast(value_t *val, kind_t k){
+bool val_setcast(val_t *val, kind_t k){
     if (!val || !kind_valid(k)) return false;
     val->cast.kind = k;
     return true;
 }
 
-bool val_castas(value_t *val, kind_t k){
+bool val_castas(val_t *val, kind_t k){
     if (!val || !kind_valid(k)) return false;
     val->self.type.kind = k;
     return true;
