5f555f6865f8bc614f12ed901f7e8d4a82124a6d
jsteeleiv <jsteele101010@gmail.com>
2026-04-19 21:23:06 -0400

    promoted sort.h to sublib; will house "children"

---
+++ c/include/jlib/jalgo/jsort.h
@@ -0,0 +1,16 @@
+#pragma once
+#ifndef JSORT_H
+#define JSORT_H
+
+#include "jbubble.h"
+#include "jmerge.h"
+#include "jquick.h"
+
+
+#endif /* JSORT_H */
+
+#ifdef JSORT_IMPL
+
+
+
+#endif /* JSORT_IMPL */
