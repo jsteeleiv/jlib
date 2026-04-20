cb5d0be6e0430142d84caca6dcb641150d97cb3f
jsteeleiv <jsteele101010@gmail.com>
2026-04-19 21:23:28 -0400

    promoted jsrch.h to sublib; will house "children"

---
+++ c/include/jlib/jalgo/jsrch.h
@@ -0,0 +1,17 @@
+#pragma once
+#ifndef JSRCH_H
+#define JSRCH_H
+
+#include "jbfs.h"
+#include "jbin.h"
+#include "jdfs.h"
+#include "jdijk.h"
+
+
+#endif /* JSRCH_H */
+
+#ifdef JSRCH_IMPL
+
+
+
+#endif /* JSRCH_IMPL */
